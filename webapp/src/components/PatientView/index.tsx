ff7012fdfe5bc4b9b7ee9313ac9c296bbb7fb642
TmPhantom <tom-maurice@gmx.de>
2021-07-09 18:03:37 +0200

    gender is converted into a symbol in the patient view

--- webapp/src/components/PatientView/index.tsx
+++ webapp/src/components/PatientView/index.tsx
@@ -1,8 +1,47 @@
 import { useState } from 'react'
-import React from 'react'
 import { useParams } from "react-router-dom";
 import './index.css';
 
+
+/**
+ * returns the respective gender symbol as svg
+ * @param gender of a patient 
+ */
+ export const transformIntoGenderSymbol = (gender?: string) => {
+    
+    const svg_width = "35";
+    const svg_height = "35";
+    const male_color = "blue";
+    const female_color = "#c00";
+
+    console.log("Gender: ", gender);
+
+    if (gender === "male") {
+        return (
+            <svg width={svg_width} height={svg_height}>
+                <circle cx="15" cy="25" r="8"  stroke={male_color} strokeWidth="2" fill="none" />
+                <line x1="19" x2="26" y1="17" y2="10" strokeWidth="2" stroke={male_color} />
+                <line x1="27" x2="16" y1="9" y2="11" strokeWidth="2" stroke={male_color} />
+                <line x1="27" x2="27" y1="9" y2="19" strokeWidth="2" stroke={male_color} />
+            </svg>
+        )
+    } else if (gender === "female") {
+        return (
+            <svg width={svg_width} height={svg_height}>
+                <circle cx="15" cy="15" r="8"  stroke={female_color} strokeWidth="2" fill="none" />
+                <line x1="15" x2="15" y1="23" y2="33" strokeWidth="2" stroke={female_color} />
+                <line x1="10" x2="20" y1="28" y2="28" strokeWidth="2" stroke={female_color} />
+            </svg>
+        )
+    } else {
+        console.log("gender is undefined");
+        return (
+            null
+        )
+    }
+}
+
+/*
 const DataCol = (props: ColProps) => {
     const {elements} = props;
 
@@ -14,19 +53,39 @@ const DataCol = (props: ColProps) => {
         </div>
     )
 }
+*/
 
 const TableRow = (props: RowProps) => {
     let {attrib_name1, attrib_name2, attrib_value1, attrib_value2} = props;
 
-    return (
-        <tr className={`border-less-tr`}>
-            <td className={`attrib-title border-less-td`}>{attrib_name1}</td>
-            <td className={`border-less-td`}>{attrib_value1}</td>
-            <td className={`attrib-title border-less-td`}>{attrib_name2}</td>
-            <td className={`border-less-td`}>{attrib_value2}</td>
-        </tr>
+    let isGenderAttrib = false;
+    if (attrib_name2 === "gender" || attrib_name2 === "Geschlecht") {
+        isGenderAttrib = true
+    }
 
-    )
+    console.log("IsAttrib: ", isGenderAttrib);
+
+    if (isGenderAttrib) {
+        return (
+            <tr className={`border-less-tr`}>
+                <td className={`attrib-title border-less-td`}>{attrib_name1}</td>
+                <td className={`border-less-td`}>{attrib_value1}</td>
+                <td className={`attrib-title border-less-td`}>{attrib_name2}</td>
+                <td className={`border-less-td`}>{transformIntoGenderSymbol(attrib_value2)}</td>
+            </tr>
+    
+        )
+    } else {
+        return (
+            <tr className={`border-less-tr`}>
+                <td className={`attrib-title border-less-td`}>{attrib_name1}</td>
+                <td className={`border-less-td`}>{attrib_value1}</td>
+                <td className={`attrib-title border-less-td`}>{attrib_name2}</td>
+                <td className={`border-less-td`}>{attrib_value2}</td>
+            </tr>
+    
+        )
+    }
 }
 
 /*
@@ -56,7 +115,7 @@ export const PatientView = () => {
 export const PatientView = () => {
     const { id }: Params = useParams();
 
-    const patient_object = {name: "Olaf", forename:"Olaf1", gender: "male", birthdate: "01.02.1997", address: "01069 Dresden Nürnberger Straße 14"}
+    let patient_object = {name: "Olaf", forename:"Olaf1", gender: "female", birthdate: "01.02.1997", address: "01069 Dresden Nürnberger Straße 14"}
 
     return (
         <div className={`patient-view-container`}>
@@ -97,5 +156,5 @@ interface RowProps {
     attrib_name1: string,
     attrib_value1: string,
     attrib_name2?: string,
-    attrib_value2?: string
+    attrib_value2?: string,
 }
