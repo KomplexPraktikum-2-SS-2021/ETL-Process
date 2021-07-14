99985330e93565eaa71aaee2e48ce5198a9c5fb6
EdgarDorausch <login@doraus.ch>
2021-07-14 21:49:02 +0200

    Now using env variables to construct hapi url

--- etl_process/src/main.py
+++ etl_process/src/main.py
@@ -2,6 +2,7 @@ import logging
 import pandas as pd
 import numpy as np
 import uuid
+import os
 
 from datetime import datetime
 from time import gmtime, strftime
@@ -10,14 +11,16 @@ from fhirclient import server
 
 from etl_process import EtlProcess
 
+HAPI_HOST = os.environ.get('HAPI_HOST', 'localhost')
+HAPI_PORT = os.environ.get('HAPI_PORT', '8080')
 
 # Create an instance
 settings = {
     'app_id' : 'my_web_app',
-    'api_base' : 'http://fhir-server:8080/fhir/'
+    'api_base' : f'http://{HAPI_HOST}:{HAPI_PORT}/fhir/'
 }
 smart = client.FHIRClient(settings=settings)
-smart_server = server.FHIRServer(client=smart, base_uri='http://fhir-server:8080/fhir/')
+smart_server = server.FHIRServer(client=smart, base_uri=f'http://{HAPI_HOST}:{HAPI_PORT}/fhir/')
 
 def main():
     logging.basicConfig(level=logging.INFO)
